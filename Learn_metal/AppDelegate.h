6ec9ea97248d124bef13799d37be11e9b05f70da
ydd <yuedd@ibobei.cn>
2019-07-09 14:37:09 +0800

    Initial Commit

---
+++ Learn_metal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Learn_metal
+//
+//  Created by ydd on 2019/7/9.
+//  Copyright © 2019 ydd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Learn_metal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Learn_metal
+//
+//  Created by ydd on 2019/7/9.
+//  Copyright © 2019 ydd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
